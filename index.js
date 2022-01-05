688d16e6e68b603234f0fd775dcebd2c41c6f4ff
Dewan Wakil Ahmed <dewanwakilahmed@gmail.com>
2022-01-06 03:02:44 +0600

    Module Export

---
+++ index.js
@@ -0,0 +1,9 @@
+// console.log("Hello from Node.js...");
+
+// const person = require("./person");
+// console.log(person);
+// console.log(person.name);
+
+const Person = require("./person");
+const person1 = new Person("John Doe", 30);
+person1.greeting();
